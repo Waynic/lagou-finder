cc9c6cd590e420f64bbfac0d711ff27cabb3d680
Q1mi <liwenzhou7@gmail.com>
2016-05-20 16:26:51 +0800

    fix lagou json data
    fix lagou json data and add some front-end test.

--- static/js/index2.js
+++ static/js/index2.js
@@ -2,7 +2,17 @@
  * Created by qimi on 2016/5/9.
  */
 (function() {
-    var map = new BMap.Map("1-map");          // 创建地图实例
-    var point = new BMap.Point(116.404, 39.915);  // 创建点坐标
-    map.centerAndZoom(point, 15);                 // 初始化地图，设置中心点坐标和地图级别
+    $(function() {
+        var map = new BMap.Map("allmap");          // 创建地图实例
+        var point = new BMap.Point(116.404, 39.915);  // 创建点坐标
+        map.centerAndZoom(point, 15);                 // 初始化地图，设置中心点坐标和地图级别
+        var top_left_control = new BMap.ScaleControl({
+            anchor: BMAP_ANCHOR_TOP_LEFT
+        });// 左上角，添加比例尺
+	    var top_left_navigation = new BMap.NavigationControl();  //左上角，添加默认缩放平移控件
+        map.addControl(top_left_control);
+        map.addControl(top_left_navigation);
+        map.setCurrentCity("北京");          // 设置地图显示的城市 此项是必须设置的
+	    map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+    })
 })();
